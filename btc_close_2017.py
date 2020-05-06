86369ae034a4bfaa74649c7b54173929d82f5caf
chen-macbook <chenyongping001@qq.com>
2020-05-06 22:54:22 +0800

    收盘日均

--- btc_close_2017.py
+++ btc_close_2017.py
@@ -33,6 +33,22 @@
 import json
 import pygal
 import math
+from itertools import groupby
+
+def draw_line(x_data, y_data, title, y_legend):
+    xy_map = []
+    for x, y in groupby(sorted(zip(x_data, y_data)),
+                        key=lambda _:_[0]):
+        y_list = [v for _, v in y]
+        xy_map.append([x, sum(y_list)/len(y_legend)])
+    x_unique, y_mean = [*zip(*xy_map)]
+    line_chart = pygal.Line()
+    line_chart.title = title
+    line_chart.x_labels = x_unique
+    line_chart.add(y_legend, y_mean)
+    line_chart.render_to_file(title+'.svg')
+    return line_chart
+
 # 将数据加载到一个列表中
 filename = 'btc_close_2017.json'
 with open(filename) as f:
@@ -56,3 +72,14 @@ line_chart._x_labels_major = dates[::N]
 close_log = [math.log10(_) for _ in close]
 line_chart.add('收盘价', close_log)
 line_chart.render_to_file('收盘价对数变线图.svg')
+
+idx_month = dates.index('2017-12-01')
+line_chart_month = draw_line(months[:idx_month], close[:idx_month], '收盘价月日均值(元)', '月日均值')
+
+idx_week = dates.index('2017-12-11')
+line_chart_week = draw_line(weeks[1:idx_week], close[1:idx_week], '收盘价周日均值(元)', '周日均值')
+
+wd =['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+weekdays_int = [wd.index(w)+1 for w in weekdays[1:idx_week]]
+line_chart_weekday = draw_line(weekdays_int, close[1:idx_week], '收盘价星期均值(元)', '星期均值')
+line_chart_weekday.x_labels = ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
