571ecff707f28633778eefbd237c58a9848255ee
chen_miBook <chenyongping001@qq.com>
2020-05-07 16:57:15 +0800

    svg形成一张网页

--- btc_close_2017.py
+++ btc_close_2017.py
@@ -65,13 +65,21 @@ for btc_dict in btc_data:
     #     date, month, week, weekday, close
     # ))
 line_chart = pygal.Line(x_label_rotation=20, show_minor_x_labels=False)
-line_chart.title = '收盘价对数变换（元）'
+line_chart.title = '收盘价(元)'
 line_chart.x_labels = dates
 N = 20 # X轴坐标每隔20天显示一次
 line_chart._x_labels_major = dates[::N]
+line_chart.add('收盘价', close)
+line_chart.render_to_file('收盘价折线图.svg')
+
+line_chart_log = pygal.Line(x_label_rotation=20, show_minor_x_labels=False)
+line_chart_log.title = '收盘价对数变换（元）'
+line_chart_log.x_labels = dates
+N = 20 # X轴坐标每隔20天显示一次
+line_chart_log._x_labels_major = dates[::N]
 close_log = [math.log10(_) for _ in close]
-line_chart.add('收盘价', close_log)
-line_chart.render_to_file('收盘价对数变线图.svg')
+line_chart_log.add('收盘价对数变换', close_log)
+line_chart_log.render_to_file('收盘价对数变线图.svg')
 
 idx_month = dates.index('2017-12-01')
 line_chart_month = draw_line(months[:idx_month], close[:idx_month], '收盘价月日均值(元)', '月日均值')
@@ -83,3 +91,13 @@ wd =['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday
 weekdays_int = [wd.index(w)+1 for w in weekdays[1:idx_week]]
 line_chart_weekday = draw_line(weekdays_int, close[1:idx_week], '收盘价星期均值(元)', '星期均值')
 line_chart_weekday.x_labels = ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+line_chart_weekday.render_to_file('收盘价星期均值(元).svg')
+
+with open('收盘价Dashboard.html', 'w', encoding='utf8') as html_file:
+    html_file.write('<html><head><title>收盘价Dashboard</title><meta charset="utf-8"></head>'
+                    '<body>\n')
+    for svg in ['收盘价折线图.svg', '收盘价对数变线图.svg', '收盘价月日均值(元).svg', '收盘价周日均值(元).svg',
+                '收盘价星期均值(元).svg']:
+        html_file.write(' <object type="image/svg+xml" data="{0}" height=500>'
+                        '</object>\n'.format(svg))
+        html_file.write('</body></html>')
